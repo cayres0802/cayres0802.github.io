c2caff4e9ce433bddd1ef8f634fd864c53e5ccde
Maria Clara Cayres <0123m.m4567@gmail.com>
2025-04-17 19:56:55 -0400

    update

--- js.js
+++ js.js
@@ -1,12 +1,10 @@
 window.onload = function (){
-    const menu_btn = document.querySelector('.hamburg');
-    const mobile_menu = document.querySelector('.mobile-nav');
-    
-    menu_btn.addEventListener('click',function(){
-        console.log('Menu clicado!');
-        menu_btn.classList.toggle('is-active');
-        mobile_menu.classList.toggle('is-active');
+	const menu_btn = document.querySelector('.hamburg');
+	const mobile_menu = document.querySelector('.mobile-nav');
 
-    });
+	menu_btn.addEventListener('click', function () {
+		menu_btn.classList.toggle('is-active');
+		mobile_menu.classList.toggle('is-active');
+	});
     
 }
